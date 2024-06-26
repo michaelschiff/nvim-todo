c3da2a21ffdf6032c6cc3bde84bb054af9292b90
michaelschiff <schiff.michael@gmail.com>
2024-06-26 00:38:03 -0700

    highlight line 0

--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -5,9 +5,8 @@ function M.setup(opt)
     return M
 end
 
--- on load colorscheme
-function M.highlight()
-  local current_win = vim.api.nvim_get_current_win()
+-- BufRead
+local function highlight()
   local current_buf = vim.api.nvim_get_current_buf()
   local line_one = vim.api.nvim_buf_get_lines(current_buf, 0, 1, false)[1]
 
@@ -18,18 +17,18 @@ end
 function M.init()
    vim.api.nvim_command('highlight default HighlightLine guifg=#ff007c gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen')
    namespace_id = vim.api.nvim_create_namespace('HighlightLineNamespace')
-   M.highlight()
+   highlight()
 end
 
 vim.api.nvim_create_augroup('HighlightLine', {})
-vim.api.nvim_create_autocmd('ColorScheme', {
+vim.api.nvim_create_autocmd('BufRead', {
   pattern = 'TODO.txt',
   group = 'HighlightLine',
   callback = M.init
 })
 
 
--- OnSave
+-- BufWritePre
 function M.printLists()
     local buffer_to_string = function()
         local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
