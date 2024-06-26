97595543a0465837fceb46d0c16dceea9bdf4055
michaelschiff <schiff.michael@gmail.com>
2024-06-25 23:31:21 -0700

    starting on todo list plugin. figuring out how to load plugin, and manage buffer content

---
+++ lua/todo/init.lua
@@ -0,0 +1,48 @@
+local M = {}
+local namespace_id
+
+function M.setup(opt)
+    return M
+end
+
+-- on load colorscheme
+function M.highlight()
+  local current_win = vim.api.nvim_get_current_win()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local line_one = vim.api.nvim_buf_get_lines(current_buf, 0, 1, false)[1]
+
+  vim.api.nvim_buf_set_extmark(current_buf, namespace_id, 0, 0, {end_row = 0, end_col = string.len(line_one), hl_group='HighlightLine'})
+end
+
+
+function M.init()
+   vim.api.nvim_command('highlight default HighlightLine guifg=#ff007c gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen')
+   namespace_id = vim.api.nvim_create_namespace('HighlightLineNamespace')
+   M.highlight()
+end
+
+vim.api.nvim_create_augroup('HighlightLine', {})
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = 'TODO.txt',
+  group = 'HighlightLine',
+  callback = M.init
+})
+
+
+-- OnSave
+function M.printLists()
+    local buffer_to_string = function()
+        local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+        return table.concat(content, "\n")
+    end
+    print(buffer_to_string())
+end
+
+vim.api.nvim_create_augroup('AutoFormatting', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = 'TODO.txt',
+  group = 'AutoFormatting',
+  callback = M.printLists 
+})
+
+return M
