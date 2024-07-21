293a805ccae01f474ba40ad70682f939a367b041
michaelschiff <schiff.michael@gmail.com>
2024-07-21 01:50:52 -0700

    complete refactor of simplified code structure. fixes info toggle bug

--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -43,61 +43,46 @@ local function highlight()
 	end
 end
 
-function M.init()
+function M.BufRead()
 	vim.api.nvim_command('highlight default HighlightLine guifg=#cf007c gui=bold ctermfg=198 cterm=bold ctermbg=darkgreen')
-	namespace_id = vim.api.nvim_create_namespace('HighlightLineNamespace')
+	namespace_id = vim.api.nvim_create_namespace('nvim-todo')
 	this_buf = vim.api.nvim_get_current_buf()
 	parse()
 	highlight()
 end
 
-vim.api.nvim_create_augroup('HighlightLine', {})
-vim.api.nvim_create_autocmd('BufRead', {
-	pattern = 'TODO.txt',
-	group = 'HighlightLine',
-	callback = M.init
-})
-
-vim.api.nvim_create_augroup('HandleCheckmark', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-	pattern = 'TODO.txt',
-	group = 'HandleCheckmark',
-	callback = function ()
-		reset()
-		parse()
-		if sections.done_section_header == nil then return end
-		local toMoveCount = 0
-		local toMove = {}
-		for i = 0, vim.api.nvim_buf_line_count(this_buf), 1 do
-			local lineContent = utils.getLine(i)
-			if utils.isChecked(lineContent) and i < sections.done_section_header.lineNumber then
-				toMoveCount = toMoveCount + 1
-				table.insert(toMove, i)
-			end
+function M.BufWritePre()
+	reset()
+	parse()
+	if sections.done_section_header == nil then return end
+	local toMoveCount = 0
+	local toMove = {}
+	for i = 0, vim.api.nvim_buf_line_count(this_buf), 1 do
+		local lineContent = utils.getLine(i)
+		if utils.isChecked(lineContent) and i < sections.done_section_header.lineNumber then
+			toMoveCount = toMoveCount + 1
+			table.insert(toMove, i)
 		end
-		-- reverse so that we dont change the line numbers of the lines we still need to delete
-		table.sort(toMove, function(x, y) return x > y end)
-		for _, v in pairs(toMove) do
-			local line = utils.getLine(v)
-			-- delete the line we are moving, this changes the line numbers of everything below, but nothing above,
-			-- which we still have to move
-			vim.api.nvim_buf_set_lines(this_buf, v, v+1, false, {nil})
-			-- dgs is further down than the line we removed, so its line number is less by one
-			sections.done_section_header.lineNumber = sections.done_section_header.lineNumber - 1;
-			-- replace the DONE heading, with the itself and the completed line.
-			-- i.e. insert the deleted line after the DONE heading
-			vim.api.nvim_buf_set_lines(this_buf, sections.done_section_header.lineNumber, sections.done_section_header.lineNumber+1, false, {utils.getLine(sections.done_section_header.lineNumber), line})
-		end
-	highlight()
 	end
-})
+	-- reverse so that we dont change the line numbers of the lines we still need to delete
+	table.sort(toMove, function(x, y) return x > y end)
+	for _, v in pairs(toMove) do
+		local line = utils.getLine(v)
+		-- delete the line we are moving, this changes the line numbers of everything below, but nothing above,
+		-- which we still have to move
+		vim.api.nvim_buf_set_lines(this_buf, v, v+1, false, {nil})
+		-- dgs is further down than the line we removed, so its line number is less by one
+		sections.done_section_header.lineNumber = sections.done_section_header.lineNumber - 1;
+		-- replace the DONE heading, with the itself and the completed line.
+		-- i.e. insert the deleted line after the DONE heading
+		vim.api.nvim_buf_set_lines(this_buf, sections.done_section_header.lineNumber, sections.done_section_header.lineNumber+1, false, {utils.getLine(sections.done_section_header.lineNumber), line})
+	end
+	highlight()
+end
 
 --TODO(michaelschiff): changing focus back to the main window should trigger toggle close of the info window
 function M.toggleDescription()
-	-- TODO(michaelschiff): this doesn't correctly handle the case where the window is closed directly by the user
-	-- e.g. if they :q in normal mode in that window. In this case PRDescriptionHandle will be non-nil, but the else
-	-- condition will error because PRDescriptionHandle points to a closed window, so closing it again fails
-	if M.PRDescriptionHandle == nil then
+	if M.PRDescriptionHandle == nil or not vim.api.nvim_win_is_valid(M.PRDescriptionHandle) then
 		local info = nil
 		local handle = io.popen(string.format("gh pr view https://github.com/Arize-ai/arize/pull/%s -q=\".title, .body\" --json=\"title,body\"", utils.getCursorWord()))
 		if handle == nil then
@@ -127,5 +112,17 @@ function M.toggleDescription()
 		M.PRDescriptionHandle = nil
 	end
 end
+
+vim.api.nvim_create_augroup('nvim-todo', {})
+vim.api.nvim_create_autocmd('BufRead', {
+	pattern = 'TODO.txt',
+	group = 'nvim-todo',
+	callback = M.BufRead
+})
+vim.api.nvim_create_autocmd('BufWritePre', {
+	pattern = 'TODO.txt',
+	group = 'nvim-todo',
+	callback = M.BufWritePre
+})
 
 return M
